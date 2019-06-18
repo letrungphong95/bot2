30b177f0b7d773671fed98ff6b66cd1c7d2ae1e5
phongtl <phongtl@vietnews24.com>
2019-06-18 17:52:13 +0700

    Up

--- bot2/settings.py
+++ bot2/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CHANNEL_LAYERS = {
     "default": {
