3804cae0785d55d3d0e482eff672d07cb0cde977
phongtl <phongtl@vietnews24.com>
2019-06-18 18:11:58 +0700

    Up

--- bot2/settings.py
+++ bot2/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'demo-bot2/static'),
+    os.path.join(BASE_DIR, '../static'),
 )
 
 # Quick-start development settings - unsuitable for production
