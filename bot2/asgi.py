c6d6484fb7b66723765bd08ca3c8bda0a245a146
phongtl <phongtl@vietnews24.com>
2019-06-24 19:19:09 +0700

    Up

--- bot2/asgi.py
+++
@@ -1,5 +0,0 @@
-import os
-import channels
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bot2.settings")
-channel_layer = channels.asgi.get_channel_layer()
--- echo/views.py
+++
@@ -1,8 +0,0 @@
-from django.views import generic
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'echo/index.html')
-
-def chat(request):
-    return render(request, 'echo/chat.html')
